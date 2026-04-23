5ccc7d3fc1f552db508067c91ba9de5ce02afe3e
hijuliancode <github-hijuliancode.designer777@passmail.com>
2026-04-22 19:22:26 -0500

    py4e: add script to read a file and print its contents in upper case, and add sample text file

---
+++ exercises/python/py4e/15-read-a-file-and-convert-its-text.py
@@ -0,0 +1,15 @@
+# Write a program that prompts for a file name,
+# then opens that file and reads through the file,
+# and print the contents of the file in upper case.
+# Use the file words.txt to produce the output below.
+
+fname = input('Enter file name: ')
+
+try:
+  fh = open(fname)
+except:
+  print('Sorry, we can\'t find a file with the name:', fname)
+
+for line in fh:
+  line = line.upper().rstrip()
+  print(line)
