ddda575f3fd9030e51cd3a8a8f2ba4d548223262
hijuliancode <github-hijuliancode.designer777@passmail.com>
2026-05-02 15:18:14 -0500

    py4e: hello course 3

--- exercises/python/py4e/012-print-text.py
+++ exercises/python/py4e/012-print-text.py
@@ -1,1 +1,6 @@
-print("Hi, I'm Julian")
+my_name = "Julian"
+number_of_courses = 3
+
+print(f"Hi, I'm {my_name}")
+print(f"This is course number {number_of_courses} that I'm taking with Dr. Chuck")
+print(f"I'm happy to be here. Nice to meet you!")
